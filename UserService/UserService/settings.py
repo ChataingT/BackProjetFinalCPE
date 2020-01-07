556efcad7062a727b6ce057d91f57a423e799597
ChataingT <38420068+ChataingT@users.noreply.github.com>
2020-01-07 17:43:00 +0100

    create activity app WIP

--- UserService/UserService/settings.py
+++ UserService/UserService/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig', # to overwrite the existing user
+    'activity.apps.ActivityConfig',
 ]
 
 
---
+++ UserService/activity/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from rest_framework.views import APIView
+
+
+class update_activity(APIView)
+# Create your views here.
